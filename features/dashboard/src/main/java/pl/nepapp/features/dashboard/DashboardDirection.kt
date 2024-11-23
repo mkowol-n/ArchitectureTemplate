c3e4ed9cde0ac5c880e14a6b84aee923b8c5621f
mKowol <mateuszkowoln@gmail.com>
2024-11-23 16:08:34 +0100

    add feature login

--- features/dashboard/src/main/java/pl/nepapp/features/dashboard/DashboardDirection.kt
+++ features/dashboard/src/main/java/pl/nepapp/features/dashboard/DashboardDirection.kt
@@ -2,7 +2,10 @@ package pl.nepapp.features.dashboard
 
 import kotlinx.serialization.Serializable
 import pl.nepapp.navigation.api.Direction
+import pl.nepapp.navigation.api.ScreenRegistry
 
 @Serializable
 object DashboardDirection: Direction
+
+abstract class DashboardScreenRegistry: ScreenRegistry<DashboardDirection>()
 
--- features/dashboard/src/main/java/pl/nepapp/features/dashboard/DashboardScreenRegistry.kt
+++
@@ -1,5 +0,0 @@
-package pl.nepapp.features.dashboard
-
-import pl.nepapp.navigation.api.ScreenRegistry
-
-abstract class DashboardScreenRegistry: ScreenRegistry<DashboardDirection>()
