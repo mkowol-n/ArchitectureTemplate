a631b7fce17e687eb6cd75237a1101843529062c
mKowol <mateuszkowoln@gmail.com>
2024-11-23 20:02:01 +0100

    add feature login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,29 +35,19 @@ android {
 dependencies {
     implementation(libs.androidx.activity.compose)
 
-//    implementation(libs.androidx.lifecycle.runtime.ktx)
-//    implementation(platform(libs.androidx.compose.bom))
-//    implementation(libs.androidx.ui)
-//    implementation(libs.androidx.ui.graphics)
-//    implementation(libs.androidx.ui.tooling.preview)
-//    implementation(libs.androidx.material3)
-    implementation(project(":features:login"))
-    implementation(project(":features:dashboard"))
     implementation(project(":core:config:http"))
     implementation(project(":core:config:shared"))
 
-    implementation(project(":features:account"))
     implementation(project(":features:main"))
-    implementation(project(":features:todo-details"))
-    implementation(project(":features:todo_list"))
-
     implementation(project(":features:login:impl"))
     implementation(project(":features:dashboard:impl"))
     implementation(project(":features:todo-details:impl"))
     implementation(project(":features:todo_list:impl"))
     implementation(project(":features:account:impl"))
-    implementation(project(":core:navigation"))
+
     implementation(project(":core:navigation:impl"))
+
+    implementation(project(":data:authorization:impl"))
 
     implementation(libs.timber)
 
@@ -66,5 +56,4 @@ dependencies {
     implementation(libs.koin.android)
     implementation(libs.koin.compose)
     implementation(libs.koin.core)
-    implementation(project(":data:authorization:impl"))
 }
--- features/main/src/main/java/pl/nepapp/main/MainScreen.kt
+++ features/main/src/main/java/pl/nepapp/main/MainScreen.kt
@@ -5,6 +5,11 @@ import org.koin.compose.koinInject
 import pl.nepapp.navigation.NepAppGraph
 
 @Composable
-fun MainScreen(nepAppGraph: NepAppGraph = koinInject()) {
+fun MainScreen() {
+    MainContent()
+}
+
+@Composable
+internal fun MainContent(nepAppGraph: NepAppGraph = koinInject()) {
     nepAppGraph.Route()
 }
