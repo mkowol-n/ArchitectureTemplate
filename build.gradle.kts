bfdc8635184b7ae6ae16c4ae75f2d37aae73f15e
mKowol <mateuszkowoln@gmail.com>
2024-11-24 23:55:06 +0100

    initial

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,20 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.version.update.checker)
+
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+// Run with dependencyUpdates
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
 }
